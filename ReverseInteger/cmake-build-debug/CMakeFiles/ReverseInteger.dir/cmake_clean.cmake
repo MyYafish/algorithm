992864c000e8a7eb0cd70364061d358644d2384a
domob-inc <wupeng@domob.cn>
2018-07-09 11:10:45 +0800

    ReverseInteger
    ReverseInteger

---
+++ ReverseInteger/cmake-build-debug/CMakeFiles/ReverseInteger.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReverseInteger.dir/main.c.o"
+  "ReverseInteger.pdb"
+  "ReverseInteger"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ReverseInteger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ReverseInteger/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <opencl-c.h>
+
+
+int reverse(int x){
+    int rev = 0;
+    while (x != 0){
+        int pop = x % 10;
+        x /= 10;
+        if(rev > INT_MAX/10 || (rev == INT_MAX  / 10 && pop > 7)){
+            return  0;
+        }
+
+        if(rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)){
+            return 0;
+        }
+
+        rev = rev * 10 + pop;
+    }
+    return rev;
+}
+
+int main() {
+    printf("Hello, World!\n");
+    reverse(1234);
+    return 0;
+}
